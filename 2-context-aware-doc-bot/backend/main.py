37a24dd349e4ca42ba1612f9e5c0acc1cfacd62f
Cristhian Richard <richardcristhian94@gmail.com>
2026-06-30 13:44:35 -0300

    2-context aware bot - backend -add Swagger UI -updated main.py to include API documentation and redirect to docs -changed function names for clarity and updated processing logic -added openapi.yaml for API specification -updated .gitignore to exclude 'results' and 'temp' directories

--- 2-context-aware-doc-bot/backend/main.py
+++ 2-context-aware-doc-bot/backend/main.py
@@ -1,14 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from flask_cors import CORS
-from parse.main import parse_repo
+from process.main import process_repo
 from model.vector_db import query_repository
-
+from swagger_ui import flask_api_doc
 
 app = Flask(__name__)
 CORS(app)
+BASE_API_PREFIX = '/api'
+flask_api_doc(app, config_path='./openapi.yaml', url_prefix='/api/docs', title='API docs')
 
-@app.route('/prompt', methods=['POST'])
-def parse():
+
+@app.route('/')
+def root_redirect():
+    return redirect('/api/docs')
+
+
+@app.route(f'{BASE_API_PREFIX}/prompt', methods=['POST'])
+def retrieve_augmented_generation():
     data = request.get_json()
     input_url = data.get('url')
     prompt = data.get('prompt')
@@ -16,12 +24,14 @@ def parse():
         return jsonify({"error": "URL is required"}), 400
 
     try:
-        parse_repo(input_url)
+        process_repo(input_url)
         context = query_repository(prompt, repo_id=input_url)
-        augmented_prompt = f"{prompt}\n\nContext:\n" + "\n".join([f"- {item['text']} (from {item['file']})" for item in context])
-        return jsonify({"response": augmented_prompt}), 200
+        response = f"{prompt}\n\nContext:\n" + "\n".join([f"- {item['text']} (from {item['file']})" for item in context])
+        return jsonify({"response": response}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
